73263a08c523472932f451f9d0981744a64488ad
CRYS <mariano.christopher123@gmail.com>
2022-08-25 20:33:43 +0800

    edit components

---
+++ components/Lottery.jsx
@@ -0,0 +1,7 @@
+import { Input, Button } from "@web3uikit/core"
+
+const Lottery = () => {
+    return <div>Lottery</div>
+}
+
+export default Lottery
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Head from "next/head"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>Lucky Trio Lottery</title>
+                <meta name="description" content="Decentralized Lottery" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider moralisAuth={false}>
+                <NotificationProvider>
+                    <Header />
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
