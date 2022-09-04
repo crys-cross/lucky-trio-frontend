e88b75bada17f5271f566982c0ea46d3a0af2500
CRYS <mariano.christopher123@gmail.com>
2022-09-04 23:52:25 +0800

    fix address error on component

--- components/Lottery.jsx
+++ components/Lottery.jsx
@@ -1,19 +1,23 @@
 import { useWeb3Contract } from "react-moralis"
-import luckyTrioAbi from "../constants/pokemonNftAbi.json"
-import networkAddresses from "../constants/pokemonNftAbi.json"
+import luckyTrioAbi from "../constants/luckyTrioAbi.json"
+import networkAddresses from "../constants/networkAddresses.json"
 import { useMoralis } from "react-moralis"
 import { useEffect, useState } from "react"
 import { ethers } from "ethers"
-import { useNotification } from "web3uikit"
-import { Button } from "web3uikit"
+import { useNotification } from "@web3uikit/core"
 import Image from "next/image"
 
 const Mint = () => {
     const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis()
     console.log(parseInt(chainIdHex))
     const chainId = parseInt(chainIdHex)
-    const lotteryAddress = chainId in networkAddresses ? networkAddresses[chainId][0] : null
-    const [entranceFee, setEntranceFee] = useState("0")
+    const lotteryAddress =
+        chainId in networkAddresses ? networkAddresses[chainId]["LotteryTrio"][0] : null
+    const [entranceFee, setEntranceFee] = useState()
+    const [entryStatus, setEntryStatus] = useState()
+    const [recentWinner, setRecentWinner] = useState()
+    const [recentWinningNumber, setRecentWinningNumber] = useState()
+    const [numberOfEntries, setNumberOfEntries] = useState()
     const dispatch = useNotification()
 
     const {
@@ -24,7 +28,7 @@ const Mint = () => {
         abi: luckyTrioAbi,
         contractAddress: lotteryAddress,
         functionName: "enterLottery",
-        params: {},
+        params: { input },
         msgValue: entranceFee,
     })
 
@@ -35,9 +39,45 @@ const Mint = () => {
         params: {},
     })
 
+    const { runContractFunction: getLotteryState } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: lotteryAddress,
+        functionName: "getLotteryState",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: lotteryAddress,
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinningNumber } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: lotteryAddress,
+        functionName: "getRecentWinningNumber",
+        params: {},
+    })
+
+    const { runContractFunction: getNumberofPlayers } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: lotteryAddress,
+        functionName: "getNumberofPlayers",
+        params: {},
+    })
+
     const updateUI = async () => {
-        const getEntranceFeeFromCall = (await getEntranceFee()).toString()
-        setEntranceFee(getEntranceFeeFromCall)
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        setEntranceFee(entranceFeeFromCall)
+        const entryStatusFromCall = (await getLotteryState()).toString()
+        setEntryStatus(entryStatusFromCall)
+        const recentWinnerFromCall = (await getRecentWinner()).toString()
+        setRecentWinner(recentWinnerFromCall)
+        const recentWinningNumberFromCall = (await getRecentWinningNumber()).toString()
+        setRecentWinningNumber(recentWinningNumberFromCall)
+        const numberOfPlayersFromCall = (await getNumberofPlayers()).toString()
+        setNumberOfEntries(numberOfPlayersFromCall)
     }
 
     useEffect(() => {
@@ -65,8 +105,11 @@ const Mint = () => {
     return (
         <section>
             <div>
-                <Image />
-                <p></p>
+                <h2>LOTTERY</h2>
+                <p>
+                    To enter this Lottery draw, please click on Enter Lottery button below and input
+                    your chosen three digit number.
+                </p>
             </div>
             <div>
                 {lotteryAddress ? (
@@ -90,6 +133,16 @@ const Mint = () => {
                         </p>
                     </div>
                 )}
+            </div>
+            <div>
+                <h4>Stats</h4>
+                <ul>
+                    <li>Entry Status: {entryStatus}</li>
+                    <li>Entrance Fee: {entranceFee}</li>
+                    <li>Recent Winner: {recentWinner}</li>
+                    <li>Recent Winning Number: {recentWinningNumber}</li>
+                    <li>Number of Entries: {numberOfEntries}</li>
+                </ul>
             </div>
         </section>
     )
